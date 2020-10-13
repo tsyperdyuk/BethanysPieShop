eb4c22895d045243890adc1f080d0123cdddd9bf
atsyperdyuk@softjournua.onmicrosoft.com <atsyperdyuk@softjournua.onmicrosoft.com>
2020-10-13 13:15:47 +0300

    first commit

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Interfaces;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List() 
+        {
+            PiesListViewModel piesViewModel = new PiesListViewModel();
+            piesViewModel.Pies = _pieRepository.AllPies;
+          
+            return View(piesViewModel);
+        }
+    }
+}
