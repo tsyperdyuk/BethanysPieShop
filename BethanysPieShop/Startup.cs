6ec8a1b401c7094a57682fcb87e227140f454ee0
atsyperdyuk@softjournua.onmicrosoft.com <atsyperdyuk@softjournua.onmicrosoft.com>
2020-10-23 16:30:17 +0300

    Finishing

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -52,6 +52,7 @@ namespace BethanysPieShop
             app.UseSession();
             app.UseRouting();
             app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
